fe28d09ae6071a7c257a599289ca86e0d9f0c3b8
jwlee2207-star <jwlee2207@gmail.com>
2026-08-29 21:55:07 +0900

    Update server.js

--- server.js
+++ server.js
@@ -233,7 +233,7 @@ setInterval(() => {
 
         for (let id in room.players) {
             let p = room.players[id];
-            const speed = 4;
+            const speed = 9;
 
             let dx = 0, dy = 0;
             if (p.keys.up) dy -= 1;
